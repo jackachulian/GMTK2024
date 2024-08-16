ba1f100994abeb7da9ce3ecd4f3ecde4ce1fa8e0
mjjm4d <threehalves1@gmail.com>
2024-08-16 17:07:14 -0500

    quick collectables system

--- Assets/Scripts/LevelData.cs
+++ Assets/Scripts/LevelData.cs
@@ -9,4 +9,17 @@ public class LevelData : MonoBehaviour
     [SerializeField] public int[] amounts;
 
     public float playerScale = 1f;
+    // for this level only
+    [System.NonSerialized] public int collectablesFound = 0;
+    public int collectablesNeeded;
+
+    public void updateCollectables(int delta)
+    {
+        collectablesFound += delta;
+        if (collectablesFound >= collectablesNeeded)
+        {
+            Debug.Log("All collectables found!");
+        }
+    }
+    
 }
