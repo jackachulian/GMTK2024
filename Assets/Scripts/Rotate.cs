336eb7cd15071a1ee487ff3c68024be9550a56c6
Jackachulian <jackachulian@gmail.com>
2024-08-16 20:37:07 -0500

    Collecable fbxs and prefabs

---
+++ Assets/Scripts/Rotate.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+namespace Animation {
+
+    /// <summary>
+    /// Script used to make an object spin around an axis.
+    /// </summary>
+    public class Rotate : MonoBehaviour
+    {
+        /// <summary> Axis to rotate around </summary> 
+        public Vector3 axis;
+        /// <summary> Speed in rotations/sec </summary>
+        public float speed;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            transform.Rotate(axis, speed * 360 * Time.unscaledDeltaTime);
+        }
+    }
+}
