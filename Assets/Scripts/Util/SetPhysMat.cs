4c889e32db09129425492f522db8e6df1928856a
mjjm4d <threehalves1@gmail.com>
2024-08-18 09:41:38 -0500

    physic material fixes

---
+++ Assets/Scripts/Util/SetPhysMat.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+// sets the phys mat of all colliders in scene
+public class SetPhysMat : MonoBehaviour
+{
+    [SerializeField] private PhysicMaterial physMat;
+    public void ReplaceAll()
+    {
+        var colliders = FindObjectsOfType<Collider>();
+
+        foreach (var collider in colliders)
+        {
+            collider.sharedMaterial = physMat;
+        }
+
+        Debug.Log("All physic materials set");
+    }
+
+}
+
+[CustomEditor(typeof(SetPhysMat))]
+public class SetPhysMatEditor : Editor 
+{
+    public override void OnInspectorGUI()
+    {
+        SetPhysMat scriptTarget = (SetPhysMat)target;
+
+        if(GUILayout.Button("Replace All"))
+            Debug.Log("bruh");
+            scriptTarget.ReplaceAll();
+    }
+}
