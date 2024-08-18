abd3a5f18c4b9571451b60dd9e4781af086043c5
mjjm4d <threehalves1@gmail.com>
2024-08-18 13:17:23 -0500

    boxcast fixes

--- Assets/Scripts/SnapToGround.cs
+++ Assets/Scripts/SnapToGround.cs
@@ -8,6 +8,9 @@ public class SnapToGround : MonoBehaviour
     private Renderer rend;
     [SerializeField] LevelData levelData;
 
+    // player layer
+    private int playerMask = 1 << 8;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -19,7 +22,13 @@ public class SnapToGround : MonoBehaviour
     {
         // set position to ground
         RaycastHit hit;
-        if (Physics.BoxCast(new Vector3(rend.bounds.center.x, rend.bounds.center.y + 2f * levelData.playerScale, rend.bounds.center.z), transform.localScale * 0.5f, Vector3.down, out hit))
+        if (Physics.BoxCast(new Vector3(rend.bounds.center.x, rend.bounds.center.y + 2f * levelData.playerScale, rend.bounds.center.z), 
+        new Vector3(rend.bounds.max.x - rend.bounds.min.x, rend.bounds.max.y - rend.bounds.min.y, rend.bounds.max.z - rend.bounds.min.z) * 0.5f, 
+        Vector3.down,
+        out hit,
+        Quaternion.identity,
+        Mathf.Infinity,
+        layerMask: ~playerMask))
         {
             Vector3 pos = transform.position;
             pos.y = hit.point.y + (rend.bounds.max.y - rend.bounds.min.y) / 2;
