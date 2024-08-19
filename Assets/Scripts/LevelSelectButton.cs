7023cf3251fcc737dbb05beede4a42c40754e7e4
mjjm4d <threehalves1@gmail.com>
2024-08-18 21:52:20 -0500

    level select functionality

--- Assets/Scripts/LevelSelectButton.cs
+++ Assets/Scripts/LevelSelectButton.cs
@@ -6,11 +6,13 @@ using UnityEngine.UI;
 
 public class LevelSelectButton : MonoBehaviour
 {
-    public bool unlocked;
+    [SerializeField] private int levelNum;
+    // private bool unlocked;
     [SerializeField] private string levelScene;
     private Button b;
 
-    void Awake(){
+    void Start(){
+        bool unlocked = PlayerPrefs.GetInt("LevelClear" + levelNum) == 1 || levelNum == 0; 
         b = GetComponent<Button>();
         if(unlocked){
             b.interactable = true;
