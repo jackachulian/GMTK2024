680db975dca9de9ee4adc2b17c4562639fe9a518
Justout <jkajustin@gmail.com>
2024-08-20 00:12:14 -0500

    Made level 7 have a different win popup

--- Assets/Scripts/LevelCompleteWindow.cs
+++ Assets/Scripts/LevelCompleteWindow.cs
@@ -3,16 +3,36 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.SceneManagement;
+using TMPro;
+using UnityEngine.Rendering;
 
 public class LevelCompleteWindow : MonoBehaviour
 {
     [SerializeField] private GameObject firstSelected;
     [SerializeField] private Animator animator;
+    [SerializeField] private TMP_Text winText1,winText2;
+    [SerializeField] private GameObject levelSelectButton, continueButton;
 
     // Start is called before the first frame update
     void Start()
     {
         gameObject.SetActive(false);
+        //Debug.Log("This is level "+ (SceneManager.GetActiveScene().buildIndex - 1));
+    }
+
+    void OnEnable(){
+        if(SceneManager.GetActiveScene().buildIndex - 1 == 7){
+            firstSelected = levelSelectButton;
+            continueButton.SetActive(false);
+            winText1.text = "  YOU";
+            winText2.text = " WIN!";
+        }
+        else{
+            continueButton.SetActive(true);
+            firstSelected = continueButton;
+            winText1.text = "LEVEL";
+            winText2.text = "CLEAR!";
+        }
     }
 
     public void OpenMenu()
